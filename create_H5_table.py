9ae2e60ae2ecaf37c8ca72095eff5a5424dd1b79
Lorenzo Cipriani <lorenzo@DESKTOP-L6EDCMR>
2022-12-23 17:42:07 +0100

    update

--- create_H5_table.py
+++ create_H5_table.py
@@ -2,9 +2,12 @@ import os
 import h5py
 import numpy as np
 
-PATH = '../EOS/compOSE/FOP(SFHoY)/'
-files = ['eos.nb', 'eos.thermo', 'eos.t', 'eos.yq']
+PATH = '/home/lorenzo/phd/NS_HOT_EOS/EOS/compOSE/FOP(SFHoY)/'
+h5_name = 'FOP(SFHoY).h5'
 
+# Files used to create the H5 file. To be generalized to all files
+files = ['eos.nb', 'eos.thermo', 'eos.t', 'eos.yq']
+# Set the number of lines to skip for each file
 skip_rows = {
                 'eos.nb' : 2,
                 'eos.thermo' : 1,
@@ -12,6 +15,7 @@ skip_rows = {
                 'eos.yq' : 2
             }
 
+# Save all the tables in a dictionary
 data = {}
 for file in files:
     data[file] = np.loadtxt(PATH + file, skiprows=skip_rows[file])
@@ -25,18 +29,28 @@ index_T    = data['eos.thermo'][:, 0].astype(int) - 1
 index_nb   = data['eos.thermo'][:, 1].astype(int) - 1
 index_ye   = data['eos.thermo'][:, 2].astype(int) - 1
 
-rho        = data['eos.nb'] * m_n     # LOGARITHMIC     MeV / fm^3
+# Baryon matter density
+rho        = np.exp(data['eos.nb']) * m_n     # e^LOGARITHMIC     MeV / fm^3
 pointsrho  = len(rho)
 
-y_e        = data['eos.yq']           # LINEAR         Adimensional
+# Electron fraction
+y_e        = data['eos.yq']                   # LINEAR         Adimensional
 pointsye   = len(y_e)
 
-temp       = data['eos.t']            # LOGARITHMIC     MeV
+# Temperature
+temp       = np.exp(data['eos.t'])            # e^LOGARITHMIC     MeV
 pointstemp = len(temp)
 
+# Map from (i_T, i_nb, i_Ye) to 1D index
 maps = lambda i,j,k : i + pointsrho * pointsye * j + pointsye * k
 
 # Find quantities and reshape them to match the H5 files in stellarcollapse.org
+# To reshape the matrices the method used is 28 times faster and equivalent to
+# for i in range(pointsye):
+#     for j in range(pointstemp):
+#         for k in range(pointsrho):
+#             reshaped_matrix[i, j, k] = original_matrix[maps(i, j, k)]
+
 pressure = data['eos.thermo'][:, 3] * data['eos.nb'][index_nb]   # MeV / fm^3
 pressure = pressure[maps(np.arange(pointsye)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :])]
 
@@ -46,14 +60,26 @@ entropy  = entropy[maps(np.arange(pointsye)[:, None, None], np.arange(pointstemp
 energy   = data['eos.thermo'][:, 9]
 energy   = energy[maps(np.arange(pointsye)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :])]
 
-ds = {  'rho'     : rho     , 'pointsrho':pointsrho, 
-        'y_e'     : y_e     , 'pointsye':pointsye, 
-        'temp'    : temp    , 'pointstemp':pointstemp, 
-        'pressure': pressure, 
-        'entropy' : entropy , 
-        'energy'  : energy
+# Adapt quantities to stellarcollapse format:
+logrho   = np.log10(rho)
+logtemp  = np.log10(temp)
+
+energy_shift = np.abs(np.min(energy)) * 1.01
+logenergy = np.log10(energy + energy_shift)
+
+logpress = np.log10(pressure)
+
+
+# Create a dictionary to automatically generate the H5 file
+ds = {  
+        'logrho'   : logrho   , 'pointsrho'   : pointsrho, 
+        'y_e'      : y_e      , 'pointsye'    : pointsye, 
+        'logtemp'  : logtemp  , 'pointstemp'  : pointstemp, 
+        'logpress' : logpress , 
+        'entropy'  : entropy  , 
+        'logenergy': logenergy, 'energy_shift': energy_shift
     }
 
-with h5py.File(PATH + "TEST_FOP(SFHoY).h5", "w") as f:
+with h5py.File(PATH + h5_name, "w") as f:
     for d in ds:
         dset = f.create_dataset(d, data=ds[d], dtype='f')
