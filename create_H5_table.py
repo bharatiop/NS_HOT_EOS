b1167cd1bc601b88ddda58464bd9fc7061658094
Lorenzo Cipriani <lorenzo@DESKTOP-L6EDCMR>
2022-12-29 09:48:22 +0100

    Added info during run

--- create_H5_table.py
+++ create_H5_table.py
@@ -40,7 +40,7 @@ h5_name = 'TEST_' + PATH.split('/')[-1] + '.h5'
 OUTPUT = os.path.join(PATH, h5_name)
 
 # Files used to create the H5 file. To be generalized to all files
-files = ['eos.nb', 'eos.thermo', 'eos.t', 'eos.yq']
+files = ['eos.nb', 'eos.thermo', 'eos.t', 'eos.yq', 'eos.compo']
 # Set the number of lines to skip for each file
 skip_rows = {
                 'eos.nb' : 2,
@@ -55,6 +55,8 @@ data = {}
 
 # Iterate through the list of file names
 for file in files:
+    print(f'Reading the file {file}...', end='\r      ')
+
     # Construct the file path
     FILE_PATH = os.path.join(PATH, file)
     
@@ -73,46 +75,76 @@ for file in files:
         # If the file is not found, print an error message
         print(f'{file} not found!')
 
+print('All files read correctly!', end = '     \n')
+print('EOS informations saved:')
+
 # Indices go from 1 to N in Fortran, from 0 to N-1 in Python
 index_T    = data['eos.thermo'][:, 0].astype(int) - 1
 index_nb   = data['eos.thermo'][:, 1].astype(int) - 1
-index_ye   = data['eos.thermo'][:, 2].astype(int) - 1
+index_yq   = data['eos.thermo'][:, 2].astype(int) - 1
 
 # Baryon matter density
 logrho     = np.log10(data['eos.nb'] * m_n)  # log_10( 10^nb * m_n )   MeV / fm^3
 pointsrho  = len(logrho)
 
-# Electron fraction
-y_e        = data['eos.yq']                  # LINEARE                 Adimensionale
-pointsye   = len(y_e)
+print('\t- rho = m_n * nb')
+
+# Charge fraction
+y_q        = data['eos.yq']                  # LINEARE                 Adimensionale
+pointsyq   = len(y_q)
+
+print('\t- Y_q')
 
 # Temperature
 logtemp    = np.log10(data['eos.t'])         # log_10( 10^T )          MeV
 pointstemp = len(logtemp)
 
+print('\t- T')
+
 # Map from (i_T, i_nb, i_Ye) to 1D index
-maps = lambda i,j,k : i + pointsrho * pointsye * j + pointsye * k
+
+def maps(i, j, k):
+    """
+    Defines a function that maps indices from the original matrix to the reshaped matrix calculating the index in the original matrix corresponding to the given indices in the reshaped matrix.
+    """
+    return i + pointsrho * pointsyq * j + pointsyq * k
+
+ 
+def reshape_array(original_matrix, pointsrho, pointstemp, pointsyq):
+    """
+    Reshapes the original matrix into the desired shape using the maps function.
+    """
+    indices = maps(np.arange(pointsyq)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :])
+    # Returns the values from the original matrix at the calculated indices
+    return np.take(original_matrix, indices)
 
 # Find quantities and reshape them to match the H5 files in stellarcollapse.org
 # To reshape the matrices the method used is faster and equivalent to
-# for i in range(pointsye):
+# for i in range(pointsyq):
 #     for j in range(pointstemp):
 #         for k in range(pointsrho):
 #             reshaped_matrix[i, j, k] = original_matrix[maps(i, j, k)]
-
 energy   = data['eos.thermo'][:, 9]                              # MeV / fm^3
-energy   = energy[maps(np.arange(pointsye)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :])]
+energy = reshape_array(energy, pointsrho, pointstemp, pointsyq)
 # Check if the energy needs to be shifted to compute the log:
 # if the minimum energy is greater than zero set the shift to zero, else it is 1% bigger than the minimum value (to avoid div by zero)
 energy_shift = np.abs(np.min([np.min(energy), 0])) * 1.01
-energy   = energy_r + energy_shift
+energy   = energy + energy_shift
+logenergy = np.log10(energy)
+
+print('\t- Internal energy ')
 
 pressure = data['eos.thermo'][:, 3] * data['eos.nb'][index_nb]   # MeV / fm^3
-pressure = pressure[maps(np.arange(pointsye)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :])]
+pressure = reshape_array(pressure, pointsrho, pointstemp, pointsyq)
+# SHIFT??
+logpress = np.log10(pressure)
+
+print('\t- Pressure')
 
 entropy  = data['eos.thermo'][:, 4] * data['eos.nb'][index_nb]   # Adimensional
-entropy  = entropy[maps(np.arange(pointsye)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :])]
+entropy = reshape_array(entropy, pointsrho, pointstemp, pointsyq)
 
+print('\t- Entropy')
 #### CS2 and GAMMA #############
 
 
@@ -149,26 +181,20 @@ for i, row in enumerate(data['eos.compo']):
         
         # Try to add the mass fraction to the mass_fractions dictionary
         # If the particle is not already in the dictionary, create a new entry
-        # with a default value of NaN for all indices
+        # with a default value of NaN for all indices and add the value
         try:
-            mass_fractions_2[f'X{particle_index[particle]}'][i] = mass_fraction
+            mass_fractions[f'X{particle_index[particle]}'][i] = mass_fraction
         except KeyError:
-            mass_fractions_2[f'X{particle_index[particle]}'] = np.full(len(data['eos.compo']), np.NaN)
-            mass_fractions_2[f'X{particle_index[particle]}'][i] = mass_fraction
+            mass_fractions[f'X{particle_index[particle]}'] = np.full(len(data['eos.compo']), np.NaN)
+            mass_fractions[f'X{particle_index[particle]}'][i] = mass_fraction
 
+print('\t- Mass fractions')
 ################################
-
-
-# Adapt quantities to stellarcollapse format:
-logrho   = np.log10(rho)
-logtemp  = np.log10(temp)
-logenergy = np.log10(energy)
-logpress = np.log10(pressure)
 
 # Create a dictionary to automatically generate the H5 file
 ds = {  
         'logrho'   : logrho   , 'pointsrho'   : pointsrho, 
-        'y_e'      : y_e      , 'pointsye'    : pointsye, 
+        'y_q'      : y_q      , 'pointsyq'    : pointsyq, 
         'logtemp'  : logtemp  , 'pointstemp'  : pointstemp, 
         'logpress' : logpress , 
         'entropy'  : entropy  , 
@@ -176,8 +202,11 @@ ds = {
     }
 
 for key in mass_fractions.keys():
-    ds[key] = mass_fractions[key][maps(np.arange(pointsye)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :]) ]
+    ds[key] = mass_fractions[key][maps(np.arange(pointsyq)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :]) ]
 
 with h5py.File(OUTPUT, "w") as f:
     for d in ds:
         dset = f.create_dataset(d, data=ds[d], dtype=np.float64)
+
+print(f'\nHDF5 file is')
+print(f'{OUTPUT}')
