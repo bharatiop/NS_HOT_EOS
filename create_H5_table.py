0bb08c4ef5a3e7cf1b67823933c33a9324dcb0c8
Lorenzo Cipriani <lorenzo@DESKTOP-L6EDCMR>
2022-12-30 10:17:48 +0100

    check and failsafes

--- create_H5_table.py
+++ create_H5_table.py
@@ -34,28 +34,53 @@ def read_table_varying_columns(FILE_PATH):
     # Return the output array
     return out_arr
 
+# Map from (i_T, i_nb, i_Ye) to 1D index
+def maps(i, j, k, pointsrho, pointsyq):
+    """
+    Defines a function that maps indices from the original matrix to the reshaped matrix calculating the index in the original matrix corresponding to the given indices in the reshaped matrix.
+    """
+    return i + pointsrho * pointsyq * j + pointsyq * k
+
+ 
+def reshape_array(original_matrix, pointsrho, pointstemp, pointsyq):
+    """
+    Reshapes the original matrix into the desired shape using the maps function.
+    """
+    indices = maps(np.arange(pointsyq)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :], pointsrho, pointsyq)
+    # Returns the values from the original matrix at the calculated indices
+    return np.take(original_matrix, indices)
+
 
 PATH = '/home/lorenzo/phd/NS_HOT_EOS/EOS/compOSE/FOP(SFHoY)'
 h5_name = 'TEST_' + PATH.split('/')[-1] + '.h5'
 OUTPUT = os.path.join(PATH, h5_name)
 
-# Files used to create the H5 file. To be generalized to all files
-files = ['eos.nb', 'eos.thermo', 'eos.t', 'eos.yq', 'eos.compo']
+# Files used to create the H5 file. 
+files = [file for file in os.listdir(PATH) if 'eos.' in file and not '.pdf' in file and not '.init' in file]
+# Check if all the mandatory files are present
+mandatory_files = ['eos.nb', 'eos.t', 'eos.yq', 'eos.thermo']
+missing_files = [file for file in mandatory_files if file not in files]
+if len(missing_files) > 0:
+    raise FileNotFoundError(f'The following mandatory files are missing: {missing_files}')
+
 # Set the number of lines to skip for each file
 skip_rows = {
                 'eos.nb' : 2,
                 'eos.thermo' : 1,
                 'eos.t' : 2,
                 'eos.yq' : 2,
-                'eos.compo' : 0
+                'eos.compo' : 0,
+                'eos.micro' : 0,
+                'eos.mr'    : 1
             }
 
 # Initialize an empty dictionary to store the data tables
 data = {}
 
+print('Starting conversion.')
 # Iterate through the list of file names
 for file in files:
-    print(f'Reading the file {file}...', end='\r      ')
+    print(f'Reading the file {file}...', end='\r')
 
     # Construct the file path
     FILE_PATH = os.path.join(PATH, file)
@@ -73,7 +98,8 @@ for file in files:
         data[file] = read_table_varying_columns(FILE_PATH)
     except KeyError:
         # If the file is not found, print an error message
-        print(f'{file} not found!')
+        print(f'{file} not found, but it is optional!')
+        
 
 print('All files read correctly!', end = '     \n')
 print('EOS informations saved:')
@@ -84,39 +110,22 @@ index_nb   = data['eos.thermo'][:, 1].astype(int) - 1
 index_yq   = data['eos.thermo'][:, 2].astype(int) - 1
 
 # Baryon matter density
-logrho     = np.log10(data['eos.nb'] * m_n)  # log_10( 10^nb * m_n )   MeV / fm^3
+logrho     = np.log10(data['eos.nb'] * m_n)  # log_10( nb * m_n )   MeV / fm^3
 pointsrho  = len(logrho)
 
-print('\t- rho = m_n * nb')
+print('\t* rho = m_n * nb')
 
 # Charge fraction
 y_q        = data['eos.yq']                  # LINEARE                 Adimensionale
 pointsyq   = len(y_q)
 
-print('\t- Y_q')
+print('\t* Y_q')
 
 # Temperature
-logtemp    = np.log10(data['eos.t'])         # log_10( 10^T )          MeV
+logtemp    = np.log10(data['eos.t'])         # log_10( T )          MeV
 pointstemp = len(logtemp)
 
-print('\t- T')
-
-# Map from (i_T, i_nb, i_Ye) to 1D index
-
-def maps(i, j, k):
-    """
-    Defines a function that maps indices from the original matrix to the reshaped matrix calculating the index in the original matrix corresponding to the given indices in the reshaped matrix.
-    """
-    return i + pointsrho * pointsyq * j + pointsyq * k
-
- 
-def reshape_array(original_matrix, pointsrho, pointstemp, pointsyq):
-    """
-    Reshapes the original matrix into the desired shape using the maps function.
-    """
-    indices = maps(np.arange(pointsyq)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :])
-    # Returns the values from the original matrix at the calculated indices
-    return np.take(original_matrix, indices)
+print('\t* T')
 
 # Find quantities and reshape them to match the H5 files in stellarcollapse.org
 # To reshape the matrices the method used is faster and equivalent to
@@ -132,64 +141,76 @@ energy_shift = np.abs(np.min([np.min(energy), 0])) * 1.01
 energy   = energy + energy_shift
 logenergy = np.log10(energy)
 
-print('\t- Internal energy ')
+print('\t* Internal energy ')
 
 pressure = data['eos.thermo'][:, 3] * data['eos.nb'][index_nb]   # MeV / fm^3
 pressure = reshape_array(pressure, pointsrho, pointstemp, pointsyq)
 # SHIFT??
 logpress = np.log10(pressure)
 
-print('\t- Pressure')
+print('\t* Pressure')
 
 entropy  = data['eos.thermo'][:, 4] * data['eos.nb'][index_nb]   # Adimensional
 entropy = reshape_array(entropy, pointsrho, pointstemp, pointsyq)
 
-print('\t- Entropy')
+print('\t* Entropy')
 #### CS2 and GAMMA #############
 
 
 ################################
 
 #### MASS FRACTIONS ############
-# Thede dictionaries might need to be generalized
-particle_index = {  0    : 'e',    10   : 'n',     11   : 'p',
-                    100  : 'Λ',    110  : 'Σ−',    111  : 'Σ0',  112  : 'Σ+',  120  : 'Ξ−', 121  : 'Ξ0', 
-                    4002 : '24He', 3002 : '23He',  3001 : '13H', 2001 : '12H', 
-                    999  : 'other'}
+if 'eos.compo' in files:
+    # Thede dictionaries might need to be generalized
+    particle_index = {  0    : 'e',    10   : 'n',     11   : 'p',
+                        100  : 'Λ',    110  : 'Σ−',    111  : 'Σ0',  112  : 'Σ+',  120  : 'Ξ−', 121  : 'Ξ0', 
+                        4002 : '24He', 3002 : '23He',  3001 : '13H', 2001 : '12H', 
+                        999  : 'other'}
 
-baryonic_number = { 0    : 0, 10   : 1, 11   : 1,
-                    100  : 1, 110  : 1, 111  : 1, 112  : 1, 120 : 1, 121 : 1,
-                    4002 : 4, 3002 : 3, 3001 : 3, 2001 : 2,}
+    baryonic_number = { 0    : 0, 10   : 1, 11   : 1,
+                        100  : 1, 110  : 1, 111  : 1, 112  : 1, 120 : 1, 121 : 1,
+                        4002 : 4, 3002 : 3, 3001 : 3, 2001 : 2,}
 
-mass_fractions = {}
-for i, row in enumerate(data['eos.compo']):
-    # Extract the number of pairs and quads from the current row
-    n_pairs = int(row[4])
-    n_quads = int(row[5 + n_pairs * 2])
+    mass_fractions = {}
+    for i, row in enumerate(data['eos.compo']):
+        # Extract the number of pairs and quads from the current row
+        n_pairs = int(row[4])
+        n_quads = int(row[5 + n_pairs * 2])
 
-    conc_iter = chain(range(5, 5 + n_pairs * 2, 2), range(6 + n_pairs * 2, 6 + n_pairs * 2 + n_quads * 4, 4))
-    # Iterate through the pairs and quads
-    for j in conc_iter:
-        # Get the particle
-        particle = int(row[j])
-        # If j is within the range of the pairs, get its mass fraction
-        if j < 5 + n_pairs * 2:
-            mass_fraction = row[j+1] * baryonic_number[particle]
-        # If j is within the range of the quads, get its mass fraction
-        else:
-            mass_fraction = row[j+1] * row[j+3]
-        
-        # Try to add the mass fraction to the mass_fractions dictionary
-        # If the particle is not already in the dictionary, create a new entry
-        # with a default value of NaN for all indices and add the value
-        try:
-            mass_fractions[f'X{particle_index[particle]}'][i] = mass_fraction
-        except KeyError:
-            mass_fractions[f'X{particle_index[particle]}'] = np.full(len(data['eos.compo']), np.NaN)
-            mass_fractions[f'X{particle_index[particle]}'][i] = mass_fraction
+        conc_iter = chain(range(5, 5 + n_pairs * 2, 2), range(6 + n_pairs * 2, 6 + n_pairs * 2 + n_quads * 4, 4))
+        # Iterate through the pairs and quads
+        for j in conc_iter:
+            # Get the particle
+            particle = int(row[j])
+            # If j is within the range of the pairs, get its mass fraction
+            if j < 5 + n_pairs * 2:
+                mass_fraction = row[j+1] * baryonic_number[particle]
+            # If j is within the range of the quads, get its mass fraction
+            else:
+                mass_fraction = row[j+1] * row[j+3]
 
-print('\t- Mass fractions')
-################################
+            # Try to add the mass fraction to the mass_fractions dictionary
+            # If the particle is not already in the dictionary, create a new entry
+            # with a default value of NaN for all indices and add the value
+            try:
+                mass_fractions[f'X{particle_index[particle]}'][i] = mass_fraction
+            except KeyError:
+                mass_fractions[f'X{particle_index[particle]}'] = np.full(len(data['eos.compo']), np.NaN)
+                mass_fractions[f'X{particle_index[particle]}'][i] = mass_fraction
+
+    # Reshape arrays
+    for key in mass_fractions.keys():
+        mass_fractions[key] = reshape_array(mass_fractions[key], pointsrho, pointstemp, pointsyq)
+
+    print('\t* Mass fractions')
+    ################################
+    #### CHEMICAL POTENTIALS #######
+    
+    
+    
+    
+    print('\t* Chemical potentials')
+    ################################
 
 # Create a dictionary to automatically generate the H5 file
 ds = {  
@@ -202,7 +223,7 @@ ds = {
     }
 
 for key in mass_fractions.keys():
-    ds[key] = mass_fractions[key][maps(np.arange(pointsyq)[:, None, None], np.arange(pointstemp)[None, :, None], np.arange(pointsrho)[None, None, :]) ]
+    ds[key] = mass_fractions[key]
 
 with h5py.File(OUTPUT, "w") as f:
     for d in ds:
